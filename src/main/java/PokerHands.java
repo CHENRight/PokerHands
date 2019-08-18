35b9fd00ce8e323a93bea80c7e3a56af41e1bcc6
CHENRight <reerchen@163.com>
2019-08-18 21:13:04 +0800

    implement player win with three of a kind,and fix the test should_player_has_pair_win

--- src/main/java/PokerHands.java
+++ src/main/java/PokerHands.java
@@ -1,43 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
 
 public class PokerHands {
 
     static Player compareCard(List<String> player1Card, List<String> player2Card) {
-        Player winner;
 
         Player player1 = new Player(player1Card);
         Player player2 = new Player(player2Card);
-        if (isHasManyPair(player1Card) && !isHasManyPair(player2Card)) {
-            winner = player1;
-        } else if (isHasOnePair(player1Card) && !isHasOnePair(player2Card)) {
-            winner = player1;
-        } else {
-            if (player1Card.get(0).charAt(0) > player2Card.get(0).charAt(0)) {
-                winner = player1;
-            } else {
-                winner = player2;
-            }
-        }
-        return winner;
-    }
 
-    static Boolean isHasOnePair(List<String> card) {
-        List<Character> cardNumber = new ArrayList<>();
-        for (String temp : card) {
-            cardNumber.add(temp.charAt(0));
-        }
-        long count = cardNumber.stream().distinct().count();
-        return count != cardNumber.size();
-    }
+        return player1.getCardLevel() > player2.getCardLevel() ? player1 : player2;
 
-    static Boolean isHasManyPair(List<String> card) {
-        Set<Character> hashSet = new HashSet<>();
-        for (String temp : card) {
-            hashSet.add(temp.charAt(0));
-        }
-        return hashSet.size() <= 3;
     }
 }
--- src/test/java/PokerHandsTest.java
+++ src/test/java/PokerHandsTest.java
@@ -25,12 +25,10 @@ public class PokerHandsTest {
     @Test
     public void should_player_has_pair_win() {
         List<String> card1 = new ArrayList<String>() {{
-            add("3C");
-            add("3H");
+            addAll(Arrays.asList("8C", "8H", "7S", "9D", "5C"));
         }};
         List<String> card2 = new ArrayList<String>() {{
-            add("3C");
-            add("4H");
+            addAll(Arrays.asList("1C", "2H", "8S", "9C", "5C"));
         }};
         Player winner = PokerHands.compareCard(card1, card2);
         Assertions.assertEquals(card1, winner.getCard());
@@ -48,7 +46,22 @@ public class PokerHandsTest {
                 addAll(Arrays.asList("8C", "8H", "7S", "9D", "5C"));
             }
         };
+        Player winner = PokerHands.compareCard(card1, card2);
+        Assertions.assertEquals(card1, winner.getCard());
+    }
 
+    @Test
+    public void should_player_win_who_has_three_of_a_kind_and_another_not() {
+        List<String> card1 = new ArrayList<String>() {
+            {
+                addAll(Arrays.asList("8C", "8H", "8S", "9D", "5C"));
+            }
+        };
+        List<String> card2 = new ArrayList<String>() {
+            {
+                addAll(Arrays.asList("8C", "8H", "7S", "7D", "5C"));
+            }
+        };
         Player winner = PokerHands.compareCard(card1, card2);
         Assertions.assertEquals(card1, winner.getCard());
     }
